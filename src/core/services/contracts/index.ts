f6b492c5b6bf943fc99d985565da473fa868b405
nSafari <nassersafari@gmail.com>
2022-09-09 02:34:54 +0200

    borrower-helper: Adding CreditLine contract call

---
+++ src/core/services/contracts/index.ts
@@ -0,0 +1,2 @@
+export { default as lineOfCreditABI } from "./lineOfCredit.json";
+export { default as ERC20ABI } from "./erc20.json";
--- src/utils/borrower-helper.ts
+++ src/utils/borrower-helper.ts
@@ -1,12 +1,82 @@
+import { Address, CreditLineService, TransactionResponse } from "@types";
+import { BigNumberish } from "ethers";
+import { BytesLike } from "@ethersproject/bytes/src.ts";
 
-interface ContractAddressesProps{
+interface ContractAddressesProps {
   lineOfCreditAddress: string;
   spigotedLineAddress: string;
   escrowAddress: string;
 }
 
-export function borrower(props: ContractAddressesProps) {
-  // use CreditLineService, ....
+export function borrower(creditLineService: CreditLineService, props: ContractAddressesProps) {
+
+  const addCredit = async (drate: BigNumberish,
+                           frate: BigNumberish,
+                           amount: BigNumberish,
+                           token: Address,
+                           lender: Address): Promise<TransactionResponse> => {
+    // set creditLineService.contract address by props.lineOfCreditAddress
+    // check if(status != LineLib.STATUS.ACTIVE) { revert NotActive(); }
+    // check mutualConsent 
+    return await creditLineService.addCredit(drate,
+      frate,
+      amount,
+      token,
+      lender
+    )
+  }
+
+  const close = async (id: BytesLike): Promise<TransactionResponse> => {
+    // set creditLineService.contract address by props.lineOfCreditAddress
+    return await creditLineService.close(id);
+  }
+
+  const setRates = async (id: BytesLike,
+                          drate: BigNumberish,
+                          frate: BigNumberish): Promise<TransactionResponse> => {
+    // set creditLineService.contract address by props.lineOfCreditAddress
+    // check mutualConsentById 
+    return await creditLineService.setRates(id,
+      drate,
+      frate,
+    )
+  }
+
+  const increaseCredit = async (id: BytesLike, amount: BigNumberish): Promise<TransactionResponse> => {
+    // set creditLineService.contract address by props.lineOfCreditAddress
+    // if(status != LineLib.STATUS.ACTIVE) { revert NotActive(); }
+    // mutualConsentById 
+    return await creditLineService.increaseCredit(id, amount)
+  }
+
+  const depositAndRepay = async (): Promise<TransactionResponse> => {
+    return Promise.resolve(<never>{});
+  }
+
+  const depositAndClose = async (): Promise<TransactionResponse> => {
+    return Promise.resolve(<never>{});
+  }
+
+  const claimAndTrade = async (): Promise<TransactionResponse> => {
+    return Promise.resolve(<never>{});
+  }
+
+  const claimAndRepay = async (): Promise<TransactionResponse> => {
+    return Promise.resolve(<never>{});
+  }
+
+  const addCollateral = async (): Promise<TransactionResponse> => {
+    return Promise.resolve(<never>{});
+  }
+
+  const removeCollateral = async (): Promise<TransactionResponse> => {
+    return Promise.resolve(<never>{});
+  }
+
+  const addSpigot = async (): Promise<TransactionResponse> => {
+    return Promise.resolve(<never>{});
+  }
+
   return {
     addCredit,
     close,
@@ -20,38 +90,4 @@ export function borrower(props: ContractAddressesProps) {
     removeCollateral,
     addSpigot
   };
-}
-
-function addCredit() {
-  
-}
-
-function close() {
-}
-
-function setRates() {
-}
-
-function increaseCredit() {
-}
-
-function depositAndRepay() {
-}
-
-function depositAndClose() {
-}
-
-function claimAndTrade() {
-}
-
-function claimAndRepay() {
-}
-
-function addCollateral() {
-}
-
-function removeCollateral() {
-}
-
-function addSpigot() {
 }
